088741412b8f3959d286594a4471c0e3244e772d
Avi Mancillas <avmancillas@gmail.com>
2021-04-24 18:25:13 -0500

    changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Week6HW
-This is Week 6 homework. We had to create a weather application using API's.
-![screenshot 1](screenshot.png)
+This is Week 6 revised homework. We had to create a weather application using API's. There were some difficulties that I would like to fix in the future.
+![screenshot 1](./weatherdashboard.png)
 [hosted-url](https://avmancillas.github.io/Week6HW/)
--- script2.js
+++ script2.js
@@ -1,88 +1,77 @@
 var key= "86fcb6cd390fc5ccbadac2ebd9459e39";
-var weeklyForecastEl=document.querySelector(".weeklyForecast");
-var cityElement = document.querySelector(".city");
-var tempElement = document.querySelector(".temperature");
-var humidityElement = document.querySelector(".humidity");
-var windElement = document.querySelector(".wind-speed");
-var uvElement = document.querySelector(".uv");
-
-// function getweeklyForecast(citysearch){
-//     var weeklyForecastEl=document.querySelector("#weeklyForecast");
-//     weeklyForecastEl.innerHTML = '<h1 class= "weeklyForecast:</h1>';
 
 
-//     for (var i = 0; i < data.list.length; i++){
-//       if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+ function getweeklyForecast(cityValue){
+     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&appid=${key}&units=imperial`)
+     .then((response) => response.json())
+     .then ((data) => {
+         console.log("success");
+         console.log(data);
+     
 
-//     }
-//     if (!citysearch){
-//         return;
-//     }
-// }
+            document.getElementById('city1').innerText = data.list[5].dt_txt;
+            document.getElementById("temperature1").innerText = `Temp: ${data.list[5].main.temp} °F`;
+            document.getElementById("humidity1").innerText= `Humidity: ${data.list[5].main.humidity} %`;
+            document.getElementById("wind-speed1").innerText= `Wind Speed: ${data.list[5].wind.speed} mph`;
+            document.getElementById("icon1").setAttribute('src',`http://openweathermap.org/img/wn/${data.list[5].weather[0].icon}@2x.png`)
+
+            document.getElementById('city2').innerText = data.list[13].dt_txt;
+            document.getElementById("temperature2").innerText = `Temp: ${data.list[13].main.temp} °F`;
+            document.getElementById("humidity2").innerText= `Humidity: ${data.list[13].main.humidity} %`;
+            document.getElementById("wind-speed2").innerText= `Wind Speed: ${data.list[13].wind.speed} mph`;
+            document.getElementById("icon2").setAttribute('src',`http://openweathermap.org/img/wn/${data.list[13].weather[0].icon}@2x.png`)
+
+            document.getElementById('city3').innerText = data.list[21].dt_txt;
+            document.getElementById("temperature3").innerText = `Temp: ${data.list[21].main.temp} °F`;
+            document.getElementById("humidity3").innerText= `Humidity: ${data.list[21].main.humidity} %`;
+            document.getElementById("wind-speed3").innerText= `Wind Speed: ${data.list[21].wind.speed} mph`;
+            document.getElementById("icon3").setAttribute('src',`http://openweathermap.org/img/wn/${data.list[21].weather[0].icon}@2x.png`)
+
+            document.getElementById('city4').innerText = data.list[29].dt_txt;
+            document.getElementById("temperature4").innerText = `Temp: ${data.list[29].main.temp} °F`;
+            document.getElementById("humidity4").innerText= `Humidity: ${data.list[29].main.humidity} %`;
+            document.getElementById("wind-speed4").innerText= `Wind Speed: ${data.list[29].wind.speed} mph`;
+            document.getElementById("icon4").setAttribute('src',`http://openweathermap.org/img/wn/${data.list[29].weather[0].icon}@2x.png`)
+
+            document.getElementById('city5').innerText = data.list[37].dt_txt;
+            document.getElementById("temperature5").innerText = `Temp: ${data.list[37].main.temp} °F`;
+            document.getElementById("humidity5").innerText= `Humidity: ${data.list[37].main.humidity} %`;
+            document.getElementById("wind-speed5").innerText= `Wind Speed: ${data.list[37].wind.speed} mph`;
+            document.getElementById("icon5").setAttribute('src',`http://openweathermap.org/img/wn/${data.list[37].weather[0].icon}@2x.png`)
+
+    }
+     )}
 
 
 
 function citySearch(cityValue){   
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&appid=${key}&units=imperial`)
-         .then((res) => res.json())
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=imperial`)
+         .then((response) => response.json())
          .then ((data) => {
          console.log("success");
          console.log(data);
+            document.getElementById('cityNow').innerText = data.name;
+            document.getElementById("temperatureNow").innerText = `Temp: ${data.main.temp} °F`;
+            document.getElementById("humidityNow").innerText= `Humidity: ${data.main.humidity} %`;
+            document.getElementById("wind-speedNow").innerText= `Wind Speed: ${data.wind.speed} mph`;
+            document.getElementById("iconNow").setAttribute('src',`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+            
 
-
-        //  for (var i = 0; i < data.list.length; i++){
-        //    if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-
-        //  }
-        })
-         .catch(error => {
-         console.log("API fail" + error);
-        });
+        }
+         
  
-    //getweeklyForecast(citySearch);
-} 
-
-
-// displayWeather(){
-//     cityElement.innerHTML = `${weather.city}`;
-//     tempElement.innerHTML = `${weather.temperature.value}`;
-//     humidityElement.innerHTML = `${weather.humidity.value}`;
-//     windElement.innerHTML = `${weather.wind.value}`;
-//     uvElement.innerHTML = `${weather.uv.value}`;
-// }
-
-// var currentHistory;
-// if (!JSON.parse(localStorage.getItem('weatherhistory'))) {
-
-// }else {
-//     currentHistory = JSON.parse(localStorage.getItem("weatherhistory"));
-// }
-
-// var historyItems =[];
-
-// function getUVIndex (lat, lon) {
-//     fetch("https://api.openweathermap.org/data/2.5/uvi?appid=86fcb6cd390fc5ccbadac2ebd9459e39&lat=${lat}&lon=${lon}"
-
-//     )
-//     .then((res) => res.json())
-//     .then((data) => {
-//         var bodyEl = document.querySelector(".card-body")
-//         var uvElement = document.querySelector("uv-index");
-//     }
-
-//     getUVIndex(data.coord.lat, data.coord.lon);
-// }
+    
+)} 
 
 function getcitySearch() {
     
-    var cityValue = document.querySelector('#search-value').value;
+    var cityValue = document.getElementById('search-value').value;
     if (cityValue){
         citySearch(cityValue);
-        makerow(cityValue);
-        document.querySelector("#search-value").value = '';
+        getweeklyForecast(cityValue);
+        document.getElementById("search-value").value = '';
         console.log("click is working")
     }
 }
 
-document.querySelector('#search-btn').addEventListener("click", function (e) { e.preventDefault(); 
-    getcitySearch});
+document.getElementById('search-btn').addEventListener("click", getcitySearch);
